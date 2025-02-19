7f820ec89076098c3a7076ccab23458d11c52ffc
Ngọc Hùng <huynhngochung12092001@gmail.com>
2025-02-19 23:01:24 +0700

    refactor: update theme

--- resources/js/@core/theme/overrides/index.ts
+++ resources/js/@core/theme/overrides/index.ts
@@ -2,7 +2,8 @@ import button from './button';
 import input from './input';
 import list from './list';
 import menu from './menu';
+import tooltip from './tooltip';
 
-const overrides = Object.assign({}, button, input, list, menu);
+const overrides = Object.assign({}, button, input, list, menu, tooltip);
 
 export default overrides;
