96f82ce10b494994667833765104b0a973149981
Ngọc Hùng <huynhngochung12092001@gmail.com>
2026-05-20 23:55:03 +0700

    refactor: check condition when update category

--- app/Http/Requests/Category/CategoryFormRequest.php
+++ app/Http/Requests/Category/CategoryFormRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Requests\Category;
 
 use App\Models\Category;
+use Closure;
 use Common\App\Enums\MediaType;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -36,6 +37,11 @@ class CategoryFormRequest extends FormRequest
                 'required',
                 'integer',
                 Rule::enum(MediaType::class),
+                function(string $attribute, mixed $value, Closure $fail): void {
+                    if ($this?->category->projects->isNotEmpty() && $value !== $this?->category->media_type->value) {
+                        $fail(__('messages')['media_type_locked']);
+                    }
+                },
             ],
         ];
     }
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Console\Commands\PruneTmpFilesCommand;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(PruneTmpFilesCommand::class)->dailyAt('5:00');
+Schedule::command(PruneTmpFilesCommand::class)->weeklyOn(Carbon::SUNDAY, '5:00');
